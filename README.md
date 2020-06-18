103e47c09db2b477950170a2a42d8dc333c58a9e
mstssk <mstssk+github@gmail.com>
2020-06-19 00:57:02 +0900

    1st Implimentation.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cleandir
+
 Cleanup directories.
---
+++ index.js
@@ -0,0 +1,43 @@
+const fsSync = require("fs");
+const fs = require("fs").promises;
+const path = require("path");
+
+const IGNORE_FILES = [".keep", ".gitkeep"];
+
+/**
+ * Cleanup directories.
+ *
+ * @param {string[]} dirs
+ */
+async function cleandir(dirs) {
+  for (const dir of dirs) {
+    await _cleandir(dir);
+  }
+}
+
+/**
+ * Cleanup specified directory.
+ *
+ * @param {string} dir
+ */
+async function _cleandir(dir) {
+  if (!fsSync.existsSync(dir)) {
+    return;
+  }
+  const stat = await fs.stat(dir);
+  if (!stat.isDirectory()) {
+    throw new Error(`'${dir}' is not a directory.`);
+  }
+  let files = await fs.readdir(dir, { withFileTypes: true });
+  files = files.filter((f) => !IGNORE_FILES.includes(f.name));
+  for (const file of files) {
+    const filePath = path.join(dir, file.name);
+    if (file.isDirectory()) {
+      await fs.rmdir(filePath, { recursive: true });
+    } else {
+      await fs.unlink(filePath);
+    }
+  }
+}
+
+exports.cleandir = cleandir;
